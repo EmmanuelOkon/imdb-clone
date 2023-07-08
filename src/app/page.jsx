e9897f65f93ec887d43eebbe07831eff4d67ec07
Emmanuel Okon <emmaudo27@gmail.com>
2023-07-08 15:39:03 +0100

    setup for tailwind css

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
+// import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="text-red-900 font-bold text-3xl">
       Home
     </main>
   );
