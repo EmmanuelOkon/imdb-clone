25a7bd3e6f0b3f07a4cce7fb79bc2f8193f01728
Emmanuel Okon <emmaudo27@gmail.com>
2023-07-09 14:57:16 +0100

    Added Navigation bar effects

---
+++ src/components/NavbarItem.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Link from "next/link";
+
+import { useSearchParams } from "next/navigation";
+import "../app/globals.css"
+
+export default function NavbarItem({ title, param }) {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get("genre");
+  return (
+    <div>
+      <Link
+        className={`hoverEffect  pb-2 ${
+          genre && genre === param && "border-b-2 border-b-amber-500"
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
+    </div>
+  );
+}
+
+{/* <Link
+        className={`-4 hover:text-amber-500 font-semibold pb-2 tracking-wider ${
+          genre && genre === param && " border-b-2 border-b-amber-500 "
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link> */}
