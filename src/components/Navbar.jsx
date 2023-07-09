60dd615cd23dc74da018b2affa5448c973799563
Emmanuel Okon <emmaudo27@gmail.com>
2023-07-09 20:01:05 +0100

    fetch data from tmdb api in the home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import NavbarItem from "./NavbarItem";
 export default function Navbar() {
   return (
     <div className="flex gap-4 justify-center dark:bg-slate-700 bg-amber-300 lg:tet-lg p-4">
-      <NavbarItem title="Trending" param="trending" />
-      <NavbarItem title="Top Rated" param="topRated" />
+      <NavbarItem title="Trending" param="fetchTrending" />
+      <NavbarItem title="Top Rated" param="fetchTopRated" />
     </div>
   );
 }
